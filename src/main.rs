a1cd5724c9035ab7b3635c68e881fe1c1618da18
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-09-02 22:01:56 +0200

    Added aliases for `peppertodo` and its subcommands

--- src/main.rs
+++ src/main.rs
@@ -9,67 +9,72 @@ fn main() {
     let mut command: Command = command!()
         .subcommand(
             Command::new("add")
+                .alias("+")
                 .about("Adds a new task")
                 .arg(
                     Arg::new("title")
                         .short('t')
                         .long("title")
                         .help("The title of the task")
-                        .required(true),
+                        .required(true)
                 )
                 .arg(
                     Arg::new("description")
                         .help("The description of the task (optional)")
                         .short('d')
                         .long("description")
-                        .required(true),
+                        .required(true)
                 ),
         )
         .subcommand(
             Command::new("list")
+                .alias("ls")
                 .about("Displays all the tasks")
                 .arg(Arg::new("filter").value_parser(EnumValueParser::<TaskStatus>::new())),
         )
         .subcommand(
             Command::new("mark")
+                .alias("!")
                 .about("Marks a task's status")
                 .arg(
                     Arg::new("target")
                         .short('t')
                         .long("title")
                         .help("The title of the task to mark")
-                        .required(true),
+                        .required(true)
                 )
                 .arg(
                     Arg::new("status")
                         .value_parser(EnumValueParser::<TaskStatus>::new())
-                        .required(true),
+                        .required(true)
                 ),
         )
         .subcommand(
             Command::new("edit")
+                .alias("ed")
                 .about("Modifies a task's title or description")
                 .arg(
                     Arg::new("target")
                         .long("target")
                         .help("The title of the task to edit")
-                        .required(true),
+                        .required(true)
                 )
                 .arg(
                     Arg::new("title")
                         .short('t')
                         .long("title")
-                        .help("The new title of the task"),
+                        .help("The new title of the task")
                 )
                 .arg(
                     Arg::new("description")
                         .short('d')
                         .long("description")
-                        .help("The new description of the task"),
+                        .help("The new description of the task")
                 ),
         )
         .subcommand(
             Command::new("delete")
+                .alias("del")
                 .about("Deletes the task with the provided title")
                 .arg(Arg::new("target").short('t').long("title").required(true)),
         );
